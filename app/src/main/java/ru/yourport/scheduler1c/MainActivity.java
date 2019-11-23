37c063137c4da1ba44d114b48e22f891b4a7be90
Dmitriy33g <dmitriy33g@gmail.com>
2019-11-23 05:07:25 +0300

    2

--- app/src/main/java/ru/yourport/scheduler1c/MainActivity.java
+++ app/src/main/java/ru/yourport/scheduler1c/MainActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.NetworkOnMainThreadException;
 
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
